2d03c82a4f9b74adf9d95dc970c6ae9456e94706
webdevabordo <jalmeida@allabordo.com>
2019-08-01 11:55:02 -0600

    Test to send to different branch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import {ADD_ARTICLE} from "../constants/ActionTypes"
 
 export function addArticle(payload){
+    console.log("Payload");
+    console.log(payload);
     return { type: "ADD_ARTICLE" , payload}
 }
